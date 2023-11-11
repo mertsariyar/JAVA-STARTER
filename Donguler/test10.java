8814f314431bacd40fd6c0ba1d2971cf5bfe42f2
Mert <101225081+mertsariyar@users.noreply.github.com>
2023-11-12 00:15:41 +0300

    Create test10.java

---
+++ Donguler/test10.java
@@ -0,0 +1,22 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class test10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Üssü alınacak sayı = ");
+        int k = input.nextInt();
+        System.out.println("üst Sayı : ");
+        int u = input.nextInt();
+        int total = 1;
+
+    
+
+        for(int i = 1; i<=u; i++) {
+            total *= k;
+            System.out.println(total);
+        }
+
+    }
+}
